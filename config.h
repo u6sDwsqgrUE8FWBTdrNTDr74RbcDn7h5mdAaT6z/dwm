d938c72d1b511989373468554a61f55555e0a04f
u6sDwsqgrUE8FWBTdrNTDr74RbcDn7h5mdAaT6z <68079809+u6sDwsqgrUE8FWBTdrNTDr74RbcDn7h5mdAaT6z@users.noreply.github.com>
2022-08-29 18:59:02 +0200

    update config.h

--- config.h
+++ config.h
@@ -60,13 +60,12 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *browser[]  = { "firejail",  "--apparmor", "--seccomp", "--caps", "--caps.drop=all", "--nonewprivs", "--private-tmp", "--private-cache", "--private-dev", "--disable-mnt", "--netfilter", "--nodvd", "--notv", "--nou2f", "--nodbus", "firefox", NULL };
 
 #include "movestack.c"
-static Key keys[] = {
+static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ 0,              XF86XK_AudioRaiseVolume, spawn,          SHCMD("pamixer -i 5; kill -44 $(pidof dwmblocks)")},
@@ -139,7 +138,7 @@ static Key keys[] = {
 
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
-static Button buttons[] = {
+static const Button buttons[] = {
 	/* click                event mask      button          function        argument */
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
